c1f1662587d3a7cf77e8ce91d7abf1a5664b141c
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-03-05 20:46:56 +0000

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,49 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "./counter-slice";
+import authReducer from "./auth-slice";
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+export default store;
+/*
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+  return state;
+};
+*/
+
+// switch (action.type) {
+//     case 'increment':
+//       return { count: state.count + 1 };
+//     case 'decrement':
+//       return { count: state.count - 1 };
+//     default:
+//       return state
